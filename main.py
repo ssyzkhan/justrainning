83831f5ca66b26b94940e4fa46990e672e7a5072
Field Ruan <fieldruan@gmail.com>
2022-12-09 11:24:48 +0800

    run function

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pygame,sys
+from settings import *
 class Main:
-  def __init__(self):
-    pygame.init()
-    self.window_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+    def __init__(self):
+        pygame.init()
+        self.window_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+    def run(self):
+        while True:
+            pass
+if __name__=='__main__':
+    main = Main()
+    main.run()
+
  
